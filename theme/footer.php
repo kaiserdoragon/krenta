bc299339f0a9dd2b0000f8a4a85331146e28980f
QUORIX\quori <kaiserdoragon1121@gmail.com>
2025-10-15 18:07:27 +0900

    フッターの原型を作成

--- theme/footer.php
+++ theme/footer.php
@@ -1,21 +1,23 @@
 <footer class="footer">
-  <div class="container">
-    <div>
-      <span>名古屋の格安・激安ウィークリー・マンスリーレンタカー店</span>
-      <a href="<?php echo home_url('/'); ?>">
-        <img src="<?php echo get_template_directory_uri(); ?>/img/common/logo.png" alt="名古屋の格安レンタカー店「Kレンタ」" width="300" height="82">
-      </a>
+  <div class="footer--inner">
+    <div class="container">
+      <div class="footer--logo">
+        <small>名古屋の格安・激安ウィークリー・マンスリーレンタカー店</small>
+        <a href="<?php echo home_url('/'); ?>">
+          <img src="<?php echo get_template_directory_uri(); ?>/img/common/logo.png" alt="名古屋の格安レンタカー店「Kレンタ」" width="300" height="82">
+        </a>
+      </div>
+      <ul>
+        <li><a href="<?php echo home_url('/price'); ?>">車種・料金</a></li>
+        <li><a href="<?php echo home_url('/flow'); ?>">ご利用の流れ</a></li>
+        <li><a href="<?php echo home_url('/store'); ?>">店舗紹介</a></li>
+        <li><a href="<?php echo home_url('/contact'); ?>">お問い合わせ</a></li>
+        <li><a href="<?php echo home_url('/'); ?>">保険・補償</a></li>
+        <li><a href="<?php echo home_url('/privacy'); ?>">プライバシーポリシー</a></li>
+      </ul>
     </div>
-    <ul>
-      <li><a href="<?php echo home_url('/price'); ?>">車種・料金</a></li>
-      <li><a href="<?php echo home_url('/flow'); ?>">ご利用の流れ</a></li>
-      <li><a href="<?php echo home_url('/store'); ?>">店舗紹介</a></li>
-      <li><a href="<?php echo home_url('/contact'); ?>">お問い合わせ</a></li>
-      <li><a href="<?php echo home_url('/'); ?>">保険・補償</a></li>
-      <li><a href="<?php echo home_url('/privacy'); ?>">プライバシーポリシー</a></li>
-    </ul>
-    <p><small>Copyright Kレンタ All Rights Reseved.</small></p>
   </div>
+  <p class="footer--copy"><small>Copyright Kレンタ All Rights Reseved.</small></p>
 </footer>
 </div>
 <?php wp_footer(); ?>
