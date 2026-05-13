4b3705d496a27d8fbb1485f0395111fe9755a030
QUORIX\quori <kaiserdoragon1121@gmail.com>
2026-05-14 07:53:15 +0900

    アーカイブ、詳細ページの作成

--- theme/single.php
+++ theme/single.php
@@ -15,40 +15,46 @@
 
       <section class="news_single">
         <div class="news_single--inner">
-          <h2><?php the_title(); ?></h2>
+          <h2 class="news_single--ttl"><?php the_title(); ?></h2>
 
-          <time datetime="<?php echo get_the_date('Y-m-d'); ?>"><?php echo get_the_date('Y.m.d'); ?></time>
-          <?php
-          $categories = get_the_category();
-          if (! empty($categories)) {
-            echo '<ul class="news_single--categories">';
-            foreach ($categories as $category) {
-              echo '<li><a href="' . esc_url(get_category_link($category->term_id)) . '"">' . esc_html($category->name) . '</a></li>';
+          <div class="news_single--date">
+            <time datetime="<?php echo get_the_date('Y年m月d日'); ?>"><?php echo get_the_date('Y年m月d日'); ?></time>
+            <?php
+            $categories = get_the_category();
+            if (! empty($categories)) {
+              echo '<ul>';
+              foreach ($categories as $category) {
+                echo '<li><a href="' . esc_url(get_category_link($category->term_id)) . '"class="news_single--categories">' . esc_html($category->name) . '</a></li>';
+              }
+              echo '</ul>';
             }
-            echo '</ul>';
-          }
-          ?>
+            ?>
+          </div>
 
-          <?php the_content(); ?>
+          <div class="news_single--content post_content">
+            <?php the_content(); ?>
+          </div>
 
           <ul class="paging">
-            <li class="paging--item paging--item-next">
-              <?php if (get_next_post()): ?>
-                <?php next_post_link('%link', '%title', false); ?>
+            <li class="paging--item paging--item-prev">
+              <?php if (get_previous_post()): ?>
+                <?php previous_post_link('%link', '前の記事へ', false); ?>
               <?php endif; ?>
-            </li>
             <li class="paging--item paging--item-gotolist">
               <a href="<?php echo esc_url(home_url('/news/')); ?>">一覧へ戻る</a>
             </li>
-            <li class="paging--item paging--item-prev">
-              <?php if (get_previous_post()): ?>
-                <?php previous_post_link('%link', '%title', false); ?>
+            <li class="paging--item paging--item-next">
+              <?php if (get_next_post()): ?>
+                <?php next_post_link('%link', '次の記事へ', false); ?>
               <?php endif; ?>
             </li>
+            </li>
           </ul>
+
         </div>
       </section>
 
-      <?php endwhile; ?><?php endif; ?>
+    <?php endwhile; ?>
+  <?php endif; ?>
 </main>
 <?php get_footer(); ?>
